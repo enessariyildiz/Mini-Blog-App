30a01ba113cd17d6d7e7bd244462d7cfa07ef76d
Enes Sariyildiz <ens.sryldz@gmail.com>
2023-12-27 16:46:07 +0300

    Entities added

---
+++ BlogApp/Entities/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entities
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string? Text { get; set; }
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
---
+++ BlogApp/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
