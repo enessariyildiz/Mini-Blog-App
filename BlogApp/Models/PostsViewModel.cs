1c732cf66740d210ed9785b713ce6764ca529c7f
Enes Sariyildiz <ens.sryldz@gmail.com>
2024-01-01 22:19:26 +0300

    View Components Added

--- BlogApp/Models/PostsViewModel.cs
+++ BlogApp/Models/PostsViewModel.cs
@@ -5,6 +5,6 @@ namespace BlogApp.Models
     public class PostsViewModel
     {
         public List<Post> Posts { get; set; } = new();
-        public List<Tag> Tags { get; set; } = new();
+        //public List<Tag> Tags { get; set; } = new();
     }
 }
--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4429ba10480e7453a8bb64eefc59922354879c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
